68f3f0df113c5737cfce9f436f0e2349438a1190
shaundiyoung <100332435+shaundiyoung@users.noreply.github.com>
2024-11-11 10:59:57 -0500

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+
+
+function expandMenu(){
+    if (window.screen.width<=("400")){
+    nav_container = document.querySelector(".nav_container");
+    nav_container.style.backgroundColor = "red";
+    console.log("here");
+    }
+    
+}
